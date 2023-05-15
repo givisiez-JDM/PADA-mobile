27b1ae1473d8d5763e222bf972bd19bdd0c77b16
joaovictorgit <victoralmeida.0216@gmail.com>
2023-05-15 13:54:07 -0300

    Add components footer

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Header from "./src/components/header/header";
+
+import Footer from "./src/components/footer/footer";
 
 export default function App() {
-  return <Header />;
+  return <Footer />;
 }
