506f1dc41d72c2efb9ed41bfa812edebc83e105e
joaovictorgit <victoralmeida.0216@gmail.com>
2023-05-18 15:23:14 -0300

    Add validate

--- src/components/form-patient-record/form-patient-record.tsx
+++ src/components/form-patient-record/form-patient-record.tsx
@@ -1,46 +1,80 @@
 import { useState } from "react";
 import {
-  ContainerIconForm,
+  ContainerErrorText,
+  ContainerFormPatientRecord,
   ContainerIconInput,
+  ContainerInput,
   IconForm,
   Input,
+  InputText,
 } from "./form-patient-record-style";
 import Icon from "react-native-vector-icons/Ionicons";
+import { Text } from "react-native";
+
 interface PatientRecordValues {
   iconName: string;
   placeholder: string;
+  handleInputChange: any;
+  state: any;
+  err: any;
 }
 
-const FormPatientRecord = ({ iconName, placeholder }: PatientRecordValues) => {
-  const [statePatient, setStatePatient] = useState({
-    name: "",
-    mail: "",
-    pass: "",
-    repeatPass: "",
-  });
-
-  const handleInputChange = (event: any) => {
-    const value = event.target.value;
-    setStatePatient({
-      ...statePatient,
-      [event.target.name]: value,
-    });
-  };
-
+const FormPatientRecord = ({
+  iconName,
+  placeholder,
+  handleInputChange,
+  state,
+  err,
+}: PatientRecordValues) => {
   return (
-    <ContainerIconForm>
+    <ContainerFormPatientRecord>
+      <ContainerInput>
+        <ContainerIconInput>
+          <IconForm>
+            <Icon name={iconName} size={20} color="#fff" />
+          </IconForm>
+        </ContainerIconInput>
+
+        <Input
+          onChangeText={handleInputChange}
+          value={state}
+          placeholder={placeholder}
+          secureTextEntry={
+            placeholder === "Senha" || placeholder === "Repetir Senha"
+              ? true
+              : false
+          }
+        />
+      </ContainerInput>
+      {err !== "" && (
+        <ContainerErrorText>
+          <Icon name="alert-circle-outline" color="#FF0000" />
+          <InputText>{err}</InputText>
+        </ContainerErrorText>
+      )}
+    </ContainerFormPatientRecord>
+    /*<ContainerIconForm>
       <ContainerIconInput>
         <IconForm>
           <Icon name={iconName} size={20} color="#fff" />
         </IconForm>
       </ContainerIconInput>
 
-      <Input
-        onChangeText={handleInputChange}
-        value={statePatient.name}
-        placeholder={placeholder}
-      />
-    </ContainerIconForm>
+      <ContainerTextInput>
+        <Input
+          onChangeText={handleInputChange}
+          value={state}
+          placeholder={placeholder}
+          secureTextEntry={
+            placeholder === "Senha" || placeholder === "Repetir Senha"
+              ? true
+              : false
+          }
+          placeholderTextColor={err ? "red" : "gray"}
+        />
+        {err !== "" && <InputText style={{ color: "red" }}>{err}</InputText>}
+      </ContainerTextInput>
+    </ContainerIconForm>*/
   );
 };
 
---
+++ src/config/validates.tsx
@@ -0,0 +1,33 @@
+interface IValidate {
+  value: any;
+  err: any;
+}
+
+export const validar = (value: any, err: any) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let error = false;
+  if (value.name === "") {
+    err.setErrorName("Preencha o campo do nome.");
+    error = true;
+  }
+  if (value.mail === "") {
+    err.setErrorMail("Preencha o campo do email.");
+    error = true;
+  }
+  if (!regex.test(value.mail)) {
+    err.setErrorMail(
+      "Preencha o campo do email de forma correta. (Ex: exemplo@gmail.com)"
+    );
+    error = true;
+  }
+  if (value.pass === "") {
+    err.setErrorPass("Preencha o campo da senha.");
+    error = true;
+  }
+  if (value.repeatPass === "") {
+    err.setErrorRepeatPass("Preencha o campo da senha.");
+    error = true;
+  }
+
+  return !error;
+};
