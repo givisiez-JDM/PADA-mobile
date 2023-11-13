6b003c36f57ea496aa25064e0b2f7f3760f7c976
joaovictorgit <victoralmeida.0216@gmail.com>
2023-11-13 14:20:51 -0300

    Update file

--- src/screen/telaSucesso/telaSucesso.tsx
+++ src/screen/telaSucesso/telaSucesso.tsx
@@ -1,10 +1,34 @@
-import { Text, View } from "react-native";
+import { Platform } from "react-native";
+import {
+  ButtonEntrar,
+  ContainerGeral,
+  ContainerSafePrincipalPacientes,
+  ImageSucesso,
+  TextButtonEntrar,
+  TextSucesso,
+  TextTitleSucesso,
+} from "./telaSucesso-style";
+import Header from "../../components/header/header";
+import FooterCodigoCelular from "../../components/footerCodigoCelular/footer-codigo-celular";
+import React from "react";
+import Sucesso from "../../assets/sucesso.png";
 
 const TelaSucesso = () => {
   return (
-    <View>
-      <Text>Tela</Text>
-    </View>
+    <ContainerSafePrincipalPacientes
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <Header back={false} />
+      <ContainerGeral>
+        <TextTitleSucesso>Recuperação de senha</TextTitleSucesso>
+        <TextSucesso>Sua nova senha foi criada com sucesso!</TextSucesso>
+        <ImageSucesso source={Sucesso} />
+        <ButtonEntrar>
+          <TextButtonEntrar>Entrar</TextButtonEntrar>
+        </ButtonEntrar>
+      </ContainerGeral>
+      <FooterCodigoCelular />
+    </ContainerSafePrincipalPacientes>
   );
 };
 
