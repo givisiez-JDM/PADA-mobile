d140d6ccb416df814c1e3fbe2cc11032bf5edde3
kyomi <104471832+kyomi-dev@users.noreply.github.com>
2024-04-08 20:35:35 -0300

    refactor: Fixed the footer width to 100%

--- src/components/Footers/footer/footer-style.ts
+++ src/components/Footers/footer/footer-style.ts
@@ -1,13 +1,14 @@
 import styled from "styled-components/native";
 
 export const ContainerFooter = styled.View`
- width:100%;
- `;
+  width:100%;
+`;
 
 export const ImageFooterCurto = styled.Image.attrs({
   resizeMode: "stretch",
 })`
   height: 120px;
+  width: 100%;
 `;
 
 export const FooterText = styled.Text`
